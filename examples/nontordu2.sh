390f921823a27abdd31385e7d0e84234d4a177fc
Timothy Bourke <Timothy.Bourke@inria.fr>
2010-09-02 17:11:14 +0000

    Add a grid, title, and better xtics to nontordu2.sh

--- examples/nontordu2.sh
+++ examples/nontordu2.sh
@@ -5,6 +5,9 @@
 
 cat | gnuplot -persist << END_GNUPLOT
 # set terminal postscript color
+set title "nontordu2"
+set grid
+set xtics 1.0
 plot "nontordu2.log" using 1:2 with lines title "x",\\
      "nontordu2.log" using 1:3 with lines title "y",\\
      "nontordu2.log" using 1:4 with lines title "z"
