235601eaed96d405b8797cf4c1528bbb812be13c
Timothy Bourke <Timothy.Bourke@inria.fr>
2011-02-04 08:59:22 +0000

    Implement the example discussed with Marc.

--- examples/misc/discontinuous.ml
+++ examples/misc/discontinuous.ml
@@ -1,35 +1,72 @@
 
 module Cvode = Cvode.Serial
 
-let f t y yd =
-  yd.{0} <- 1.0
+let t = 0
+let x = 1
 
-let g t y gout =
-  gout.{0} <- if t < 2.000012345 then -1.0 else 1.0
+let thresh1 = 0.987
+let thresh2 = 1.987
 
-let y = Cvode.Carray.of_array [| 0.0 |]
+let t_i = 0.0
+let x_i = 0.0
+
+let max_sim_t = 5.0
+
+(*
+       der t = 1.0 init t_i
+   and r = if t < thresh1 then 1.0
+           else if t < thresh2 then -. 1.0
+           else 2.0
+   and der x = r init x_i
+
+ *)
+
+let f t_s y yd =
+  let r =
+    if y.{t} < thresh1 then 1.0
+    else if y.{t} < thresh2 then -1.0
+    else 2.0
+  in
+  yd.{t} <- 1.0;
+  yd.{x} <- r
+
+(* z = up(t -. thresh2) *)
+
+let g t_s y gout =
+  gout.{0} <- y.{t} -. thresh2
+
+(* simulation *)
+
+let y = Cvode.Carray.of_array [| t_i; x_i |]
 
 let s = Cvode.init Cvode.Adams Cvode.Functional f (1, g) y
 let rootdata = Cvode.Roots.create 1
 
-let _ = Cvode.set_stop_time s 5.0
+let _ = Cvode.set_stop_time s max_sim_t
+
+exception Done
 
 let _ =
+  Printf.printf "time\t\t t\t\t x\n";
+  Printf.printf "----------------------------------------\n";
   Cvode.Carray.print_with_time 0.0 y;
-  let t = ref 0.1 in
-  let keep_going = ref true in
-  while !keep_going do
-    let (t', result) = Cvode.normal s !t y in
-        Cvode.Carray.print_with_time t' y;
-        t := t' +. 0.1;
-        match result with
-        | Cvode.RootsFound -> begin
-              Cvode.get_root_info s rootdata;
-              Cvode.Roots.print rootdata
-            end
-        | Cvode.StopTimeReached -> keep_going := false
-        | Cvode.Continue -> ();
-  done
+  try
+    let i = ref 0 in
+    while true do
+      let (t', result) = Cvode.one_step s max_sim_t y in
+
+      Printf.printf "\nstep %3d.\n" !i;
+      incr i;
+
+      Cvode.Carray.print_with_time t' y;
+      Printf.printf "(step size = %e)\n" (Cvode.get_last_step s);
+        
+      match result with
+      | Cvode.RootsFound -> print_endline "** root found"
+      | Cvode.StopTimeReached -> raise Done
+      | Cvode.Continue -> ()
+    done
+  with Done -> ()
 
 let _ = Cvode.free s
 
