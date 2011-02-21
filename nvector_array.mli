8288cdb1f619f6745d0375bfaf4f171cea3d8028
Timothy Bourke <Timothy.Bourke@inria.fr>
2011-02-21 10:15:51 +0000

    Add testing targets for the Sundials examples.

--- nvector_array.mli
+++ nvector_array.mli
@@ -16,10 +16,3 @@ module Bigarray :
 
 include ARRAY_NVECTOR with type t = float array
 
-(*
-val array_nvec_ops  : float array Nvector.Mutable.nvector_ops
-val make            : int -> float -> float array Nvector.nvector
-val wrap            : float array -> float array Nvector.nvector
-val data            : 'a Nvector.nvector -> 'a
-*)
-
