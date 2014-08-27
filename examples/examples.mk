c7e69cef1fdce4330a9bd43cf9bd8c8cfca062e9
Jun Inoue <jun.lambda@gmail.com>
2014-08-28 00:43:47 +0200

    Tentative perf implementation (serial only)

--- examples/examples.mk
+++ examples/examples.mk
@@ -29,7 +29,7 @@ ENABLED_EXAMPLES=$(EXAMPLES) $(if $(LAPACK_ENABLED),$(LAPACK_EXAMPLES)) \
 	         $(if $(MPI_ENABLED),$(MPI_EXAMPLES))
 SUNDIALSLIB_DEPS=$(foreach x,$(SUNDIALSLIB),$(SRCROOT)/$x)
 
-## Top-level targets
+## Testing correctness
 all: $(ENABLED_EXAMPLES:.ml=.byte) $(ENABLED_EXAMPLES:.ml=.opt)
 
 tests.byte: $(ENABLED_EXAMPLES:.ml=.byte)
@@ -76,8 +76,7 @@ $(LAPACK_TESTS): lapack-tests.%.log: $(LAPACK_EXAMPLES:.ml=.%.diff)
 	cat $@
 	@! grep '^[0-9]' $@ | grep -q '^[^0]'
 
-
-## Build / execution rules
+# Build / execution rules
 
 # Keep outputs of tests that crashed.  Those outputs are not
 # automatically updated afterwards, so unless you update the test or
@@ -136,6 +135,61 @@ CAML_LD_LIBRARY_PATH:=$(SRCROOT):$(CAML_LD_LIBRARY_PATH)
 %.self.diff: %.byte.out %.opt.out
 	@diff -u $^ > $@ || true
 
+## Performance measurement
+
+PERF=perf.opt.log perf.byte.log
+
+.PHONY: $(PERF)
+
+AWK ?= awk
+
+avg='BEGIN { s=n=0 }				\
+     { ++n; s+=$$1; }				\
+     END { printf "%.2f\n", s/n; }'
+ratio =									\
+    ml=`$(AWK) $(avg) $<`;						\
+    c=`$(AWK) $(avg) $(word 2,$^)`;					\
+    quot=`$(AWK) "END { printf \"%.2f\", ($$ml/$$c) }" /dev/null`;	\
+    echo "$(SUBDIR)/$(<:.time=)	$$ml	$$c	$$quot"
+
+$(PERF): perf.%.log: $(ENABLED_EXAMPLES:.ml=.%) $(ENABLED_EXAMPLES:.ml=.%.perf)
+	cat $(filter %.perf,$^) > $@
+	@$(if $(findstring $@,$(MAKECMDGOALS)), \
+	      echo "# <name>	<OCaml time>	<C time>	<OCaml time / C time>")
+	$(if $(findstring $@,$(MAKECMDGOALS)),cat $@)
+
+%.opt.perf: %.opt.time %.sundials.time
+	@$(call ratio) > $@
+
+%.byte.perf: %.byte.time %.sundials.time
+	@$(call ratio) > $@
+
+# How many times to measure each example.
+PERF_REPS ?= 3
+
+measure =							\
+	i=$(PERF_REPS);						\
+	while test $$i -gt 0 &&					\
+	    time -f '%e' $(1) 2>&1 >/dev/null | tee -a $(2); do	\
+	    i=`expr $$i - 1`;					\
+	done; test $$i -eq 0
+
+.SECONDARY: $(ALL_EXAMPLES:.ml=.byte.time) $(ALL_EXAMPLES:.ml=.opt.time)	\
+	    $(ALL_EXAMPLES:.ml=.sundials.time)					\
+	    $(ALL_EXAMPLES:.ml=.byte.perf) $(ALL_EXAMPLES:.ml=.opt.perf)
+
+%.sundials.time: $(EXAMPLESROOT)/$(SUBDIR)/%
+	@echo -$<
+	@($(call measure, $<, $@))
+
+%.opt.time: %.opt
+	@echo -$<
+	@($(call measure, ./$<, $@)) 2> $@
+
+%.byte.time: %.byte
+	@echo -$<
+	@($(call measure, ./$<, $@)) 2> $@
+
 ## Dependences to files living outside of the examples directory
 
 # Just remind the user to recompile the library rather than actually
@@ -162,6 +216,10 @@ clean:
 	-@rm -f $(ALL_EXAMPLES:.ml=.annot)
 	-@rm -f $(ALL_EXAMPLES:.ml=.byte.diff) $(ALL_EXAMPLES:.ml=.opt.diff)
 	-@rm -f $(ALL_EXAMPLES:.ml=.self.diff)
+	-@rm -f $(ALL_EXAMPLES:.ml=.byte.time) $(ALL_EXAMPLES:.ml=.opt.time)
+	-@rm -f $(ALL_EXAMPLES:.ml=.sundials.time)
+	-@rm -f $(ALL_EXAMPLES:.ml=.byte.perf) $(ALL_EXAMPLES:.ml=.opt.perf)
 	-@rm -f tests.log lapack-tests.log tests.self.log
 	-@rm -f tests.byte.log lapack-tests.byte.log
 	-@rm -f tests.opt.log lapack-tests.opt.log
+	-@rm -f perf.byte.log perf.opt.log
