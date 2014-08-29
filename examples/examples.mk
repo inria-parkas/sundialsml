7ced1bb2929318c6c3f583b7433eeb86afeecbf6
Jun Inoue <jun.lambda@gmail.com>
2014-08-29 20:35:23 +0200

    Use OCaml script to measure performance.

--- examples/examples.mk
+++ examples/examples.mk
@@ -8,7 +8,7 @@
 #   Path to the root of the source tree.
 # * SUBDIR
 #   Which subdirectory's Makefile included this examples.mk file.
-#   cvode/serial, ida/parallel, etc.
+#   cvode/serial, ida/parallel, etc.  No / allowed at the end.
 # * EXAMPLES, LAPACK_EXAMPLES, MPI_EXAMPLES
 #   List of .ml files in $(SUBDIR) that:
 #     - don't use lapack or MPI
@@ -27,7 +27,10 @@ DIVIDER = "---------------------------------------------------------------------
 ALL_EXAMPLES=$(EXAMPLES) $(LAPACK_EXAMPLES) $(MPI_EXAMPLES)
 ENABLED_EXAMPLES=$(EXAMPLES) $(if $(LAPACK_ENABLED),$(LAPACK_EXAMPLES)) \
 	         $(if $(MPI_ENABLED),$(MPI_EXAMPLES))
+SERIAL_EXAMPLES=$(EXAMPLES) $(LAPACK_EXAMPLES)
 SUNDIALSLIB_DEPS=$(foreach x,$(SUNDIALSLIB),$(SRCROOT)/$x)
+
+UTILS=$(SRCROOT)/examples/utils
 
 ## Testing correctness
 all: $(ENABLED_EXAMPLES:.ml=.byte) $(ENABLED_EXAMPLES:.ml=.opt)
@@ -88,14 +91,12 @@ $(LAPACK_TESTS): lapack-tests.%.log: $(LAPACK_EXAMPLES:.ml=.%.diff)
 # if you modify the binding but forget to recompile it.  Is there a
 # way to protect against the latter without being too invasive?
 
-$(EXAMPLES:.ml=.byte) $(LAPACK_EXAMPLES:.ml=.byte):	\
- %.byte: %.ml $(SRCROOT)/$(USELIB).cma
+$(SERIAL_EXAMPLES:.ml=.byte): %.byte: %.ml $(SRCROOT)/$(USELIB).cma
 	$(OCAMLC) $(OCAMLFLAGS) -o $@ \
 	    $(INCLUDES) -I $(SRCROOT) -dllpath $(SRCROOT) bigarray.cma unix.cma \
 	    $(USELIB).cma $<
 
-$(EXAMPLES:.ml=.opt) $(LAPACK_EXAMPLES:.ml=.opt):	\
- %.opt: %.ml $(SRCROOT)/$(USELIB).cmxa
+$(SERIAL_EXAMPLES:.ml=.opt): %.opt: %.ml $(SRCROOT)/$(USELIB).cmxa
 	$(OCAMLOPT) $(OCAMLOPTFLAGS) -o $@ \
 	    $(INCLUDES) -I $(SRCROOT) bigarray.cmxa unix.cmxa \
 	    $(USELIB).cmxa $<
@@ -137,58 +138,86 @@ CAML_LD_LIBRARY_PATH:=$(SRCROOT):$(CAML_LD_LIBRARY_PATH)
 
 ## Performance measurement
 
+# How many times to measure each example.  Each measurement repeats
+# the example several times to make it run long enough.
+PERF_REPS ?= 3
+
+# At least how long each measurement should take.  If this value is
+# too low, the measurement will be unreliable.
+MIN_TIME ?= 1
+
 PERF=perf.opt.log perf.byte.log
 
 .PHONY: $(PERF)
 
-AWK ?= awk
-
-avg='BEGIN { s=n=0 }				\
-     { ++n; s+=$$1; }				\
-     END { printf "%.2f\n", s/n; }'
-ratio =									\
-    ml=`$(AWK) $(avg) $<`;						\
-    c=`$(AWK) $(avg) $(word 2,$^)`;					\
-    quot=`$(AWK) "END { printf \"%.2f\", ($$ml/$$c) }" /dev/null`;	\
-    echo "$(SUBDIR)/$(<:.time=)	$$ml	$$c	$$quot"
-
-$(PERF): perf.%.log: $(ENABLED_EXAMPLES:.ml=.%) $(ENABLED_EXAMPLES:.ml=.%.perf)
-	cat $(filter %.perf,$^) > $@
-	@$(if $(findstring $@,$(MAKECMDGOALS)), \
-	      echo "# <name>	<OCaml time>	<C time>	<OCaml time / C time>")
-	$(if $(findstring $@,$(MAKECMDGOALS)),cat $@)
-
-%.opt.perf: %.opt.time %.sundials.time
-	@$(call ratio) > $@
-
-%.byte.perf: %.byte.time %.sundials.time
-	@$(call ratio) > $@
-
-# How many times to measure each example.
-PERF_REPS ?= 3
-
-measure =							\
-	i=$(PERF_REPS);						\
-	while test $$i -gt 0 &&					\
-	    time -f '%e' $(1) 2>&1 >/dev/null | tee -a $(2); do	\
-	    i=`expr $$i - 1`;					\
-	done; test $$i -eq 0
-
-.SECONDARY: $(ALL_EXAMPLES:.ml=.byte.time) $(ALL_EXAMPLES:.ml=.opt.time)	\
-	    $(ALL_EXAMPLES:.ml=.sundials.time)					\
+.SECONDARY: $(ALL_EXAMPLES:.ml=.byte.time) $(ALL_EXAMPLES:.ml=.opt.time)     \
+	    $(ALL_EXAMPLES:.ml=.sundials) $(ALL_EXAMPLES:.ml=.sundials.time) \
 	    $(ALL_EXAMPLES:.ml=.byte.perf) $(ALL_EXAMPLES:.ml=.opt.perf)
 
-%.sundials.time: $(EXAMPLESROOT)/$(SUBDIR)/%
-	@echo -$<
-	@($(call measure, $<, $@))
+$(UTILS)/perf: $(UTILS)/perf.ml
+	$(OCAMLOPT) -o $@ unix.cmxa $<
 
-%.opt.time: %.opt
-	@echo -$<
-	@($(call measure, ./$<, $@)) 2> $@
+$(PERF): perf.%.log: $(ENABLED_EXAMPLES:.ml=.%) $(ENABLED_EXAMPLES:.ml=.%.perf)
+	@grep '^#' $(lastword $^) >$@
+	@sed -n '/^[^#]/ p' $(filter %.perf,$^) >> $@
+	cat $@
 
-%.byte.time: %.byte
-	@echo -$<
-	@($(call measure, ./$<, $@)) 2> $@
+%.opt.perf: %.opt.time %.sundials.time $(UTILS)/perf
+	$(UTILS)/perf -s $(<:.opt.time=) $(filter-out $(UTILS)/perf,$^) > $@
+
+%.byte.perf: %.byte.time %.sundials.time
+	$(UTILS)/perf -s $(<:.byte.time=) $(filter-out $(UTILS)/perf,$^) > $@
+
+# .sundials should be run before .opt or .byte - it determines NUM_REPS.
+%.sundials.time: %.sundials $(UTILS)/perf
+	$(UTILS)/perf -m $(MIN_TIME) $(PERF_REPS) ./$< \
+	    | tee $@ 
+
+%.opt.time: %.opt %.sundials.time $(UTILS)/perf
+	$(UTILS)/perf -i $(word 2,$^) $(PERF_REPS) ./$< \
+	    | tee $@
+
+%.byte.time: %.byte %.sundials.time $(UTILS)/perf
+	$(UTILS)/perf -i $(word 2,$^) $(PERF_REPS) ./$< \
+	    | tee $@
+
+# Compilation of C examples with environment-handling wrappers.
+
+MODULE=$(word 1,$(subst /, ,$(SUBDIR)))
+
+ifeq ($(MODULE),cvode)
+EG_CFLAGS=$(CVODE_CFLAGS)
+EG_LDFLAGS=$(CVODE_LDFLAGS)
+else ifeq ($(MODULE),cvodes)
+EG_CFLAGS=$(CVODES_CFLAGS)
+EG_LDFLAGS=$(CVODES_LDFLAGS)
+else ifeq ($(MODULE),ida)
+EG_CFLAGS=$(IDA_CFLAGS)
+EG_LDFLAGS=$(IDA_LDFLAGS)
+else ifeq ($(MODULE),idas)
+EG_CFLAGS=$(IDAS_CFLAGS)
+EG_LDFLAGS=$(IDAS_LDFLAGS)
+else ifeq ($(MODULE),kinsol)
+EG_CFLAGS=$(KINSOL_CFLAGS)
+EG_LDFLAGS=$(KINSOL_LDFLAGS)
+endif
+
+$(ALL_EXAMPLES:.ml=.sundials.c): %.sundials.c: $(EXAMPLESROOT)/$(SUBDIR)/%.c \
+					       $(UTILS)/sundials_wrapper.c.in
+	@if grep -q 'main *( *void *)' $< || grep -q 'main *( *)' $<;	    \
+	 then main_args=;						    \
+	 else main_args="argc, argv";					    \
+	 fi;								    \
+	 sed -e 's#@sundials_src_name@#$<#' -e "s#@main_args@#$$main_args#" \
+	   $(UTILS)/sundials_wrapper.c.in > $@
+
+$(SERIAL_EXAMPLES:.ml=.sundials): %.sundials: %.sundials.c
+	$(CC) -o $@ -I $(EXAMPLESROOT)/$(SUBDIR) \
+	    $(EG_CFLAGS) $< $(LIB_PATH) $(EG_LDFLAGS) $(LAPACK_LIB)
+
+$(MPI_EXAMPLES:.ml=.sundials): %.sundials: %.sundials.c
+	$(MPICC) -o $@ -I $(EXAMPLESROOT)/$(SUBDIR) \
+	    $(EG_CFLAGS) $< $(LIB_PATH) $(EG_LDFLAGS) $(LAPACK_LIB)
 
 ## Dependences to files living outside of the examples directory
 
@@ -217,7 +246,8 @@ clean:
 	-@rm -f $(ALL_EXAMPLES:.ml=.byte.diff) $(ALL_EXAMPLES:.ml=.opt.diff)
 	-@rm -f $(ALL_EXAMPLES:.ml=.self.diff)
 	-@rm -f $(ALL_EXAMPLES:.ml=.byte.time) $(ALL_EXAMPLES:.ml=.opt.time)
-	-@rm -f $(ALL_EXAMPLES:.ml=.sundials.time)
+	-@rm -f $(ALL_EXAMPLES:.ml=.sundials) $(ALL_EXAMPLES:.ml=.sundials.c)
+	-@rm -f $(ALL_EXAMPLES:.ml=.sundials.time) $(ALL_EXAMPLES:.ml=.reps)
 	-@rm -f $(ALL_EXAMPLES:.ml=.byte.perf) $(ALL_EXAMPLES:.ml=.opt.perf)
 	-@rm -f tests.log lapack-tests.log tests.self.log
 	-@rm -f tests.byte.log lapack-tests.byte.log
