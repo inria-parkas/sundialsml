5b9cad824c22861ba9cc86795672dc3983c9c285
Timothy Bourke <tim@tbrk.org>
2014-11-20 09:31:26 +0100

    Fix the Kinsol skeleton code.

--- examples/ocaml/skeletons/kinsol_skel.ml
+++ examples/ocaml/skeletons/kinsol_skel.ml
@@ -1,17 +1,42 @@
-(* Set vector with initial guess
-   The length of this vector determines the problem size. *)
-let u = Nvector_array.wrap [| 0.0; 0.0; 0.0 |]
-(* Create and initialize a solver session
-   This will also initialize the specified linear solver. *)
-let s = Kinsol.init (Kinsol.Spgmr callbacks) f u
-(* Set optional inputs,
-   Call any of the [set_*] functions to change solver and linear solver
-   parameters from their defaults. *)
-set_num_max_iters s 500;
+(* 1. Define a system function. *)
+let pi, e = 4. *. atan 1., exp 1.
+let sysf u r =
+  r.{0} <- 0.5 *. sin(u.{0}*.u.{1}) -. 0.25 *. u.{1} /. pi -. 0.5 *. u.{0};
+  r.{1} <- (1. -. 0.25/.pi)*.(exp(2.*.u.{0})-.e) +. e*.u.{1}/.pi -. 2.*.e*.u.{0};
+  r.{2} <- u.{2} -. u.{0} +. 0.25;
+  r.{3} <- u.{3} -. u.{0} +. 1.0;
+  r.{4} <- u.{4} -. u.{1} +. 1.5;
+  r.{5} <- u.{5} -. u.{1} +. 2.0 *. pi
 
-(* Solve problem *)
-let result = Kinsol.solve s u u_scale f_scale
-(* Get optional outputs
-   Call any of the [get_*] functions to examine solver and linear solver
-   statistics. *)
-let fnorm = Kinsol.get_func_norm s in ...
+(* 2. Set vector with initial guess.
+      The length of this vector determines the problem size. *)
+let ud = Sundials.RealArray.of_list [ 0.25; 1.5; 0.; -0.75; 0.; 1.5 -. 2. *.pi ]
+let u = Nvector_serial.wrap ud
+
+(* 3. Create and initialize a solver session.
+      This will initialize a specific linear solver. *)
+let s = Kinsol.init (Kinsol.Dls.dense None) sysf u;;
+
+(* 4. Set optional inputs, e.g.,
+      call [set_*] functions to change solver parameters. *)
+let c = Sundials.RealArray.of_list [
+   Sundials.Constraint.unconstrained;
+   Sundials.Constraint.unconstrained;
+   Sundials.Constraint.geq_zero;
+   Sundials.Constraint.leq_zero;
+   Sundials.Constraint.geq_zero;
+   Sundials.Constraint.leq_zero ] in
+Kinsol.set_constraints s (Nvector_serial.wrap c);
+Kinsol.set_func_norm_tol s 1.0e-5;
+Kinsol.set_scaled_step_tol s 1.0e-5;
+Kinsol.set_max_setup_calls s 1;;
+
+(* 5. Solve the problem. *)
+let snv = Nvector_serial.make (Sundials.RealArray.length ud) 1.0 in
+ignore (Kinsol.solve s u false snv snv);
+
+Printf.printf "%8.5g %8.6g\n" ud.{0} ud.{1};;
+
+(* 6. Get optional outputs,
+      call the [get_*] functions to examine solver statistics. *)
+let fnorm = Kinsol.get_func_norm s;;
