bf4c96aa8283264e973a249f23e08f3fbcda5d54
Jun Inoue <jun.lambda@gmail.com>
2014-11-20 16:42:35 +0100

    Remove stale quickcheck code

--- tests/quickcheck_sundials_toplevel.ml
+++
@@ -1,4 +0,0 @@
-(* This module should be loaded when using the sundials part of quickcheck from
-   the top level.  It sets a flag so that certain problems can be avoided.  See
-   the comment where inhibit_quickcheck_main is defined.  *)
-Quickcheck_sundials.inhibit_quickcheck_main := true
