e28e5357c611d8f36fe399cb7a204aafdaf67fac
Timothy Bourke <tim@tbrk.org>
2015-01-08 07:09:38 +0100

    Example exhibiting misuse of the solver interface.
    Initializing a session with one type of linear solver (e.g., Diag or Dls)
    and then using the functions of another (e.g., Spils) can cause invalid
    typecasts and memory accesses in Sundials/C.

---
+++ examples/ocaml/misc/ls_badmem.ml
@@ -0,0 +1,31 @@
+(* Compile with:
+    ocamlc -o ls_badmem.byte -I +sundials -dllpath +sundials \
+           bigarray.cma unix.cma sundials.cma ls_badmem.ml
+ *)
+
+let printf = Printf.printf
+
+let f t y yd = yd.{0} <- 1.0
+
+(* simulation *)
+
+let y = Sundials.RealArray.of_array [| 0.0 |]
+let y_nv = Nvector_serial.wrap y
+
+let s1 = Cvode.init Cvode.Adams
+                    (Cvode.Newton
+                      (Cvode.Spils.spgmr Cvode.Spils.prec_none))
+                    Cvode.default_tolerances
+                    f 0.0 y_nv;;
+
+let i = Cvode.Spils.get_num_lin_iters s1;;
+printf "Spils.get_num_lin_iters s1 = %d\n" i;;
+
+let s2 = Cvode.init Cvode.Adams
+                    (Cvode.Newton Cvode.Diag.solver)
+                    Cvode.default_tolerances
+                    f 0.0 y_nv;;
+
+let i = Cvode.Spils.get_num_lin_iters s2;;
+printf "Spils.get_num_lin_iters s2 = %d\n" i;;
+
