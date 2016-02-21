393666f05ad0c55d4371db34b4a313afc79faa86
Timothy Bourke <tim@tbrk.org>
2016-02-21 21:21:42 +0100

    Fix some klu and superlumt linking problems

--- src/kinsol/kinsol_klu.ml
+++ src/kinsol/kinsol_klu.ml
@@ -21,7 +21,7 @@ type ordering =
    | Natural
 
 external c_klu : 'k serial_session -> int -> int -> unit
-  = "c_kin_klu_init"
+  = "c_kinsol_klu_init"
 
 let klu f nnz session nv =
   session.neqs <- Sundials.RealArray.length (Nvector.unwrap nv);
@@ -30,21 +30,21 @@ let klu f nnz session nv =
   c_klu session session.neqs nnz
 
 external c_set_ordering : 'k serial_session -> ordering -> unit
-  = "c_kin_klu_set_ordering"
+  = "c_kinsol_klu_set_ordering"
 
 let set_ordering session ordering =
   ls_check_klu session;
   c_set_ordering session ordering
 
 external c_reinit : 'k serial_session -> int -> int -> bool -> unit
-  = "c_kin_klu_reinit"
+  = "c_kinsol_klu_reinit"
 
 let reinit session n nnz realloc =
   ls_check_klu session;
   c_reinit session n nnz realloc
 
 external c_get_num_jac_evals : 'k serial_session -> int
-  = "c_kin_klu_get_num_jac_evals"
+  = "c_kinsol_klu_get_num_jac_evals"
 
 let get_num_jac_evals session =
   ls_check_klu session;
