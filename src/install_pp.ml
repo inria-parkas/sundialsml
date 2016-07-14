f2aa70a0823362ae43a5555a14d984fc0cb871ec
Timothy Bourke <tim@tbrk.org>
2016-07-14 16:34:21 +0200

    Script to install (most) pretty printers

---
+++ src/install_pp.ml
@@ -0,0 +1,11 @@
+#require "sundialsml";;
+#install_printer Sundials.RealArray.pp;;
+#install_printer Sundials.RealArray2.pp;;
+#install_printer Sundials.LintArray.pp;;
+#install_printer Sundials.Roots.pp;;
+#install_printer Sundials.RootDirs.pp;;
+#install_printer Dls.DenseMatrix.pp;;
+#install_printer Dls.BandMatrix.pp;;
+#install_printer Sls.SparseMatrix.pp;;
+#install_printer Nvector_serial.pp;;
+
