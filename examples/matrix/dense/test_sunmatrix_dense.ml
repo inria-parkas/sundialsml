f3df981364efac72339615d808760e0eede54e29
Jun Inoue <jun.lambda@gmail.com>
2018-07-02 19:05:23 +0900

    Minor fix in dense matrix example

--- examples/matrix/dense/test_sunmatrix_dense.ml
+++ examples/matrix/dense/test_sunmatrix_dense.ml
@@ -161,8 +161,8 @@ let main () =
   (* SUNMatrix Tests *)
   fails += Test.test_sunmatgetid a Matrix.Dense 0;
   fails += Test.test_sunmatclone a 0;
-  fails += Test.test_sunmatzero a 0;
   fails += Test.test_sunmatcopy a 0;
+  fails += Test.test_sunmatzero a 0;
   fails += Test.test_sunmatscaleadd a i 0;
   if square then
     fails += Test.test_sunmatscaleaddi a i 0;
