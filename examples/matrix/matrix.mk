c8055d9b3d2a36fde98e9ba85df65d54d37f1072
Timothy Bourke <tim@tbrk.org>
2020-08-25 17:00:09 +0200

    Copy/build locally for nvector and matrix examples
    The previous implementation that built common files in the parent
    directories fails from time to time when using `make -j`. Multiple
    builds of the parent files are started in parallel and overwrite one
    another's files. Building in the child directories then becomes
    unreliable (the child build starts thinking that the parent files are
    ready, but in the meantime another parent build has already started
    rewriting those parent files).

--- examples/matrix/matrix.mk
+++ examples/matrix/matrix.mk
@@ -2,24 +2,32 @@
 # wrapper around examples.mk that adds build rules for
 # test_matrix.ml.
 
-EXTRA_DEPS = ../test_matrix_ml.o ../test_matrix.cmo
+EXTRA_DEPS = test_matrix_ml.o test_matrix.cmo
 
-FILES_TO_CLEAN=../test_matrix_ml.o		\
-	       ../test_matrix.o			\
-	       ../test_matrix.annot		\
-	       ../test_matrix.cmi		\
-	       ../test_matrix.cmo		\
-	       ../test_matrix.cmx		\
-	       ../test_matrix.cmt
+FILES_TO_CLEAN=test_matrix.ml		\
+	       test_matrix_ml.c		\
+	       test_matrix_ml.o		\
+	       test_matrix.o		\
+	       test_matrix.annot	\
+	       test_matrix.cmi		\
+	       test_matrix.cmo		\
+	       test_matrix.cmx		\
+	       test_matrix.cmt
 
 OCAMLFLAGS += -I .. -custom
 OCAMLOPTFLAGS += -I ..
 
 include ../../examples.mk
 
-../test_matrix.cmo: ../test_matrix.ml
+# use local copies to avoid problems with make -j
+test_matrix.ml: ../test_matrix.ml
+	cp $< $@
+test_matrix_ml.c: ../test_matrix_ml.c
+	cp $< $@
+
+test_matrix.cmo: test_matrix.ml
 	$(OCAMLC) $(OCAMLFLAGS) -c $(SUBDIRS:%=-I $(SRC)/%) -o $@ $<
 
-../test_matrix.cmx: ../test_matrix.ml
+test_matrix.cmx: test_matrix.ml
 	$(OCAMLOPT) $(OCAMLOPTFLAGS) -c $(SUBDIRS:%=-I $(SRC)/%) -o $@ $<
 
--- examples/nvector/nvector.mk
+++ examples/nvector/nvector.mk
@@ -2,22 +2,31 @@
 # wrapper around examples.mk that adds build rules for
 # test_nvector.ml.
 
-EXTRA_DEPS =../test_nvector_ml.o ../test_nvector.cmo
+EXTRA_DEPS = test_nvector_ml.o test_nvector.cmo
 
-FILES_TO_CLEAN=../test_nvector_ml.o		\
-	       ../test_nvector.o		\
-	       ../test_nvector.annot		\
-	       ../test_nvector.cmi		\
-	       ../test_nvector.cmo		\
-	       ../test_nvector.cmx
+FILES_TO_CLEAN=test_nvector.ml		\
+	       test_nvector_ml.c	\
+	       test_nvector_ml.o	\
+	       test_nvector.o		\
+	       test_nvector.annot	\
+	       test_nvector.cmi		\
+	       test_nvector.cmo		\
+	       test_nvector.cmx		\
+	       test_nvector.cmt
 
 OCAMLFLAGS += -I .. -custom
 OCAMLOPTFLAGS += -I ..
 
 include ../../examples.mk
 
-../test_nvector.cmo: ../test_nvector.ml
+# use local copies to avoid problems with make -j
+test_nvector.ml: ../test_nvector.ml
+	cp $< $@
+test_nvector_ml.c: ../test_nvector_ml.c
+	cp $< $@
+
+test_nvector.cmo: test_nvector.ml
 	$(OCAMLC) $(OCAMLFLAGS) -c $(SUBDIRS:%=-I $(SRC)/%) -o $@ $<
 
-../test_nvector.cmx: ../test_nvector.ml
+test_nvector.cmx: test_nvector.ml
 	$(OCAMLOPT) $(OCAMLOPTFLAGS) -c $(SUBDIRS:%=-I $(SRC)/%) -o $@ $<
