5ee9e60a5a7d019f46d0ba2b63d74b4b3476a3a1
Timothy Bourke <tim@tbrk.org>
2021-09-15 20:27:43 +0200

    Fix ARKode MRIStep skeleton

--- examples/ocaml/skeletons/arkode_mri_skel.ml
+++ examples/ocaml/skeletons/arkode_mri_skel.ml
@@ -3,25 +3,25 @@ module MRIStep = Arkode.MRIStep
 module ARKStep = Arkode.ARKStep
 
 (* 1. Define right-hand-side functions. *)
-let fslow t y yd = yd.{0} <- y.{1}
-                ; yd.{1} <- 0.0 (* XXX *)
-let ffast t y yd = yd.{1} <- -9.81
-                ; yd.{0} <- 0.0 (* XXX *)
+let fslow t y yd = yd.{0} <- y.{1};
+                   yd.{1} <- 0.0
+let ffast t y yd = yd.{0} <- 0.0;
+                   yd.{1} <- -9.81
 
 (* 2. Optionally define a root function. *)
 let g t y gout = gout.{0} <- 1.0 -. y.{0}
 
 (* 3. Set vector of initial values.
       The length of this vector determines the problem size. *)
-let yd = RealArray.of_list [ 10.0; 0.0 ]
-let y = Nvector_serial.wrap yd
+let y = RealArray.of_list [ 10.0; 0.0 ]
+let ynv = Nvector_serial.wrap y
 
 (* 4. Create an ARKStep solver for the fast (inner) integration *)
-let sf = ARKStep.(init (explicit ffast) default_tolerances 0.0 y)
+let sf = ARKStep.(init (explicit ffast) default_tolerances 0.0 ynv)
 
 (* 5. Create and initialize a solver session. *)
-let slowstep = 0.1
-let s = MRIStep.(init sf default_tolerances fslow ~roots:(1, g) ~slowstep 0.0 y);;
+let slowstep = 0.01
+let s = MRIStep.(init sf default_tolerances fslow ~roots:(1, g) ~slowstep 0.0 ynv);;
 
 (* 6. Set optional inputs, e.g.,
       call [set_*] functions to change solver parameters. *)
@@ -31,12 +31,13 @@ MRIStep.set_all_root_directions s RootDirs.Increasing;;
 (* 7. Advance the solution in time,
       by repeatedly calling [solve_normal] or [solve_one_step]. *)
 let rec go (t, r) =
-  Printf.printf "% .10e\t% .10e\t% .10e\n" t yd.{0} yd.{1};
+  Printf.printf "% .10e\t% .10e\t% .10e\n" t y.{0} y.{1};
   match r with
-  | MRIStep.Success -> go (MRIStep.solve_normal s (t +. 0.5) y)
+  | MRIStep.Success -> go (MRIStep.solve_normal s (t +. 0.5) ynv)
   | MRIStep.RootsFound -> begin
-        yd.{1} <- -0.8 *. yd.{1};
-        MRIStep.reinit s t y;
+        y.{1} <- -0.8 *. y.{1};
+        MRIStep.reinit s t ynv;
+        ARKStep.reinit sf t ynv;
         go (t, MRIStep.Success)
       end
   | MRIStep.StopTimeReached -> ();;
